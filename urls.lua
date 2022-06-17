a59ef7c65abf7bb747a50571875ed6178134238d
James Hsiao <matchstick@gmail.com>
2022-06-16 23:08:31 -0400

    added url processing

--- urls.lua
+++ urls.lua
@@ -1,5 +1,5 @@
 -- open Chrome with a specific named Profile
-function chromeWithProfile(profile, url)
+function openChromeWithProfile(profile, url)
   local t = hs.task.new(
     "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
     nil,
@@ -8,14 +8,69 @@ function chromeWithProfile(profile, url)
   t:start()
 end
 
--- IN PROGRESS (May just give up and stick with finicky.js)
-function handleHTTP(scheme, host, params, url, sender)
-  local intuit = "Profile 3"
-  local mailchimp = "Profile 2"
-  local mchosts = { "rsglab", "mailchimp", "mcpeeps", "meet.google.com" }
-  local inthosts = { "intuit", "alight.com", "perksatwork.com", "degreed.com",
-                     "www.concursolutions.com", "align.ustream.tv", "outlook.office365.com" }
-
-  if hs.eventtap.checkKeyboardModifiers()["cmd"] then chromeWithProfile(mailchimp, url) end
-  if hs.eventtap.checkKeyboardModifiers()["shift"] then chromeWithProfile(intuit, url) end
+-- Spotify
+function openSpotify(url)
+  local t = hs.task.new(
+    "/Applications/Spotify.app/Contents/MacOS/Spotify",
+    nil,
+    function() return false end,
+    { "--uri=" .. url })
+  t:start()
 end
+
+
+function handleHTTP(scheme, host, params, url, sender)
+  local mailchimp = {
+    pf    = "Profile 2",
+    hosts = { "rsglab", "mailchimp", "mcpeeps", "meet.google.com" },
+    paths = { "mailchimp" }
+  }
+  local intuit = {
+    pf    = "Profile 3",
+    hosts = { "intuit", "alight.com", "perksatwork.com", "degreed.com",
+                "www.concursolutions.com", "align.ustream.tv", "outlook.office365.com" },
+    paths = { "intuit" }
+  }
+
+  if string.match(host, "open.spotify.com") then
+    openSpotify(url)
+    return true
+  end
+
+  if hs.eventtap.checkKeyboardModifiers()["cmd"] or
+    hs.fnutils.some(mailchimp["hosts"], function(el)
+                      if string.match(host, el) then return true end end)
+  then
+    openChromeWithProfile(mailchimp["pf"], url)
+    return true
+  end
+
+  if hs.eventtap.checkKeyboardModifiers()["shift"] or
+    hs.fnutils.some(intuit["hosts"],
+                    function(el)
+                      if string.match(host, el) then return true end end)
+  then
+    openChromeWithProfile(intuit["pf"], url)
+    return true
+  end
+
+  if hs.fnutils.some(mailchimp["paths"],
+                     function(el)
+                       if string.match(url, el) then return true end end)
+  then
+    openChromeWithProfile(mailchimp["pf"], url)
+    return true
+  end
+
+  if hs.fnutils.some(intuit["paths"],
+                     function(el)
+                       if string.match(url, el) then return true end end)
+  then
+    openChromeWithProfile(intuit["pf"], url)
+    return true
+  end
+
+  hs.urlevent.openURLWithBundle(url, "org.mozilla.firefox")
+end
+
+hs.urlevent.httpCallback = handleHTTP
