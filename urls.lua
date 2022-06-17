f635e85032cb4d2b0d3c5b401646af4d577c80ca
James Hsiao <matchstick@gmail.com>
2022-06-17 12:03:33 -0400

    updated urls to handle just URLs not hosts/paths

--- urls.lua
+++ urls.lua
@@ -38,61 +38,50 @@ end
 
 function handleHTTP(scheme, host, params, url, sender)
   local mailchimp = {
-    pf    = "Profile 2",
-    hosts = { "rsglab", "mailchimp", "mcpeeps", "meet.google.com" },
-    paths = { "mailchimp" }
+    pf = "Profile 2",
+    matches = { "rsglab", "mailchimp", "mcpeeps", "meet.google.com" },
   }
   local intuit = {
-    pf    = "Profile 3",
-    hosts = { "intuit", "alight.com", "perksatwork.com", "degreed.com",
+    pf = "Profile 3",
+    matches = { "intuit", "alight.com", "perksatwork.com", "degreed.com",
                 "www.concursolutions.com", "align.ustream.tv", "outlook.office365.com" },
-    paths = { "intuit" }
   }
 
-  url = cleanURL(url)
+  -- remove tracking and other unwanted URL cruft
+  if string.match(url, "?") then url = cleanURL(url) end
+
+  -- Spotify
   if string.match(host, "open.spotify.com") then
     openSpotify(url)
     return true
   end
 
+  -- Zoom
   if string.match(url, "zoom.us/j") then
     hs.urlevent.openURLWithBundle(url, "us.zoom.xos")
     return true
   end
 
+  -- Mailchimp
   if hs.eventtap.checkKeyboardModifiers()["cmd"] or
-    hs.fnutils.some(mailchimp["hosts"], function(el)
-                      if string.match(host, el) then return true end end)
+    hs.fnutils.some(mailchimp["matches"], function(el)
+                      if string.match(url, el) then return true end end)
   then
     openChromeWithProfile(mailchimp["pf"], url)
     return true
   end
 
+  -- Intuit
   if hs.eventtap.checkKeyboardModifiers()["shift"] or
-    hs.fnutils.some(intuit["hosts"],
+    hs.fnutils.some(intuit["matches"],
                     function(el)
-                      if string.match(host, el) then return true end end)
+                      if string.match(url, el) then return true end end)
   then
     openChromeWithProfile(intuit["pf"], url)
     return true
   end
 
-  if hs.fnutils.some(mailchimp["paths"],
-                     function(el)
-                       if string.match(url, el) then return true end end)
-  then
-    openChromeWithProfile(mailchimp["pf"], url)
-    return true
-  end
-
-  if hs.fnutils.some(intuit["paths"],
-                     function(el)
-                       if string.match(url, el) then return true end end)
-  then
-    openChromeWithProfile(intuit["pf"], url)
-    return true
-  end
-
+  -- Default
   hs.urlevent.openURLWithBundle(url, "org.mozilla.firefox")
 end
 
