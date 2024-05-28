c40df80880698ec888c7f7f04ac1f94210c251fc
James Hsiao <matchstick@gmail.com>
2024-05-28 16:21:11 -0400

    enabled use with U3417W

--- screens.lua
+++ screens.lua
@@ -6,14 +6,16 @@ local obj = {}
 obj.mbpScreen = "Built-in Retina Display"
 obj.samsungScreen = "S23C570"
 obj.homeScreen = "DELL U3419W"
-obj.miamiScreen = "HP S2031"
-obj.mcScreen = "DELL U3417W"
+obj.mia1Screen = "DELL U3417W"
+obj.mia2Screen = "DELL S2422HG"
 obj.atl1Screen = "C34J79x"
 obj.atl2Screen = "VG27A"
 
 obj.screens = {}
 obj.mc = nil
 obj.mainDdcID = nil
+
+obj.defaultInput = "27" -- USB-C
 
 function obj.init()
   if hs.screen(obj.homeScreen) then
@@ -22,8 +24,13 @@ function obj.init()
       function(exitCode, output, err) obj.mainDdcID = string.gsub(output, "%s", "") end
     ):start():waitUntilExit()
     obj.screens = { hs.screen(obj.homeScreen), hs.screen(obj.samsungScreen) }
-  elseif hs.screen(obj.miamiScreen) then
-    obj.screens = { hs.screen(obj.miamiScreen), hs.screen(obj.mbpScreen) }
+  elseif hs.screen(obj.mia1Screen) then
+    hs.task.new(
+      os.getenv("HOME") .. "/.hammerspoon/bin/dell_id",
+      function(exitCode, output, err) obj.mainDdcID = string.gsub(output, "%s", "") end
+    ):start():waitUntilExit()
+    obj.screens = { hs.screen(obj.mia1Screen), hs.screen(obj.mia2Screen) }
+    obj.defaultInput = "15" -- DP
   elseif hs.screen(obj.mcScreen) then
     obj.screens = { hs.screen(obj.mcScreen), hs.screen(obj.mbpScreen) }
   elseif hs.screen(obj.atl1Screen) then
@@ -42,7 +49,7 @@ function obj.init()
 end
 
 function obj.switchMonitorInput(caffeinate)
-  local cInput = "27"
+  local cInput = obj.defaultInput
   local nInput
 
   hs.task.new(
@@ -50,7 +57,7 @@ function obj.switchMonitorInput(caffeinate)
     function(exitCode, output, err) cInput = string.gsub(output, "%s", "") end
   ):start():waitUntilExit()
 
-  if cInput == "27" then
+  if cInput == obj.defaultInput then
     nInput = "17" -- switch to HDMI 1
   else
     nInput = "27" -- switch to USB-C
